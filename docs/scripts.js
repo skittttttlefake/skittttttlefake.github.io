3c5a9ee310c6d324958f1316a1d86d1958f54447
skittttttlefake <song_08182021@qq.com>
2023-01-14 12:30:22 +0800

    a

---
+++ docs/scripts.js
@@ -0,0 +1,28 @@
+var 我超噢批 = document.querySelectorAll("p");
+
+window.onload = 远控点地LL;
+
+
+
+function 远控点地LL(){
+setInterval(yiyiyitest,500);
+}
+function 后门点塞pipi(服主, 服主2){
+    min = Math.ceil(服主);
+    max = Math.floor(服主2);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+// function cqsber(){
+    // var 疮穷=document.createElement("p");
+	// var 传奇公文本=document.createTextNode("疮穷啥b");
+	// 疮穷.appendChild(传奇公文本);
+	// document.body.appendChild(疮穷);}
+
+function yiyiyitest(){
+    我超噢批.forEach(function (element){
+        var 从咯人 = 后门点塞pipi(0,255);
+        var 从咯人2 = 后门点塞pipi(0,255);
+        var 从咯人3 = 后门点塞pipi(0,255);
+        var rgb = "rgb(" + 从咯人 + "," + 从咯人2 + "," + 从咯人3 + ")";
+        element.style.color = rgb;})
+}
